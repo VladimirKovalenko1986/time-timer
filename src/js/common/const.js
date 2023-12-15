6bd4651a2e1fdb1c792212a1eeb823bc2c02dc06
Vladimir Kovalenko <dante77758@gmail.com>
2023-12-15 22:50:20 +0200

    add files

---
+++ src/js/common/const.js
@@ -0,0 +1,12 @@
+const refs = {
+  dayWeek: document.querySelector('.date-day'),
+  dayMounth: document.querySelector('.date'),
+  month: document.querySelector('.date-month'),
+  year: document.querySelector('.date-year'),
+  digitalClock: document.querySelector('.digital-clock'),
+  arrowSeconds: document.querySelector('.clock-seconds__arrow'),
+  arrowMinutes: document.querySelector('.clock-minutes__arrow'),
+  arrowHours: document.querySelector('.clock-hours__arrow'),
+};
+
+export { refs };
--- src/js/timer-rusin.js
+++ src/js/timer-rusin.js
@@ -1,8 +1,57 @@
-const refs = {
-  dayWeek: document.querySelector('.date-day'),
-  dateMounth: document.querySelector('.date'),
-  month: document.querySelector('.date-month'),
-  year: document.querySelector('.date-year'),
-};
+import { refs } from './common/const';
 
-const currentTime = new Date();
+const namesOfMonth = [
+  'Січень',
+  'Лютий',
+  'Березень',
+  'Квітень',
+  'Травень',
+  'Червень',
+  'Липень',
+  'Серпень',
+  'Вересень',
+  'Жовтень',
+  'Листопад',
+  'Грудень',
+];
+
+const arrDay = [
+  'Неділя',
+  'Понеділок',
+  'Вівторок',
+  'Середа',
+  'Четвер',
+  "П'ятниця",
+  'Субота',
+];
+
+setInterval(() => {
+  const currentTime = new Date();
+  const currentDay = arrDay[currentTime.getDay()];
+  const currentDate = currentTime.getDate();
+  const currentMonth = namesOfMonth[currentTime.getMonth()];
+  const currentYear = currentTime.getFullYear();
+  const currentHour = currentTime.getHours();
+  const currentMinets = currentTime.getMinutes();
+  const currentSeconds = currentTime.getSeconds();
+
+  const formatTime = `
+  ${currentHour.toString().padStart(2, '0')} : 
+  ${currentMinets.toString().padStart(2, '0')} : 
+  ${currentSeconds.toString().padStart(2, '0')}`;
+
+  const changeSecond = (360 / 60) * currentSeconds;
+  const changeMinutes = (360 / 60) * currentMinets;
+  const changHours = (360 / 12) * currentHour + (360 / 12 / 60) * currentMinets;
+
+  refs.dayWeek.textContent = currentDay;
+  refs.dayMounth.textContent = currentDate;
+  refs.month.textContent = currentMonth;
+  refs.year.textContent = currentYear;
+
+  refs.digitalClock.textContent = `Поточний час: ${formatTime}`;
+
+  refs.arrowSeconds.style.transform = `rotate(${changeSecond}deg)`;
+  refs.arrowMinutes.style.transform = `rotate(${changeMinutes}deg)`;
+  refs.arrowHours.style.transform = `rotate(${changHours}deg)`;
+}, 1000);
