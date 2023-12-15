1a237083f9abdd2bbffc47efd9b60f57ed3c9f33
VladimirKovalenko1986 <110353203+VladimirKovalenko1986@users.noreply.github.com>
2023-12-15 20:50:43 +0000

    Deploying to gh-pages from @ VladimirKovalenko1986/time-timer@6bd4651a2e1fdb1c792212a1eeb823bc2c02dc06 🚀

--- partials/timer-rusin.7c6e8237.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".date-day"),document.querySelector(".date"),document.querySelector(".date-month"),document.querySelector(".date-year"),new Date;
-//# sourceMappingURL=timer-rusin.7c6e8237.js.map
--- partials/timer-rusin.cd22429e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".date-day"),document.querySelector(".date"),document.querySelector(".date-month"),document.querySelector(".date-year"),new Date;
-//# sourceMappingURL=timer-rusin.cd22429e.js.map
